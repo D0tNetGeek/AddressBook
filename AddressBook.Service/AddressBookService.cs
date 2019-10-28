8ca55ffa1b9d6bdad091377dae54688d24bb6aeb
Vinnie Malvi <vinod.malvi@yahoo.co.uk>
2019-10-28 03:08:02 +0000

    Implemented service interface

--- AddressBook.Service/AddressBookService.cs
+++ AddressBook.Service/AddressBookService.cs
@@ -1,8 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using AddressBook.Service.Models;
 
 namespace AddressBook.Service
 {
-    public class AddressBookService
+    public class AddressBookService : IAddressBookService
     {
+        public int CountPersonsBasedOnGender(Gender gender, IList<ContactDto> contacts)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetDaysBetweenTwoPersons(ContactDto contact1, ContactDto contact2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ContactDto GetOldestPerson(IList<ContactDto> contacts)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
