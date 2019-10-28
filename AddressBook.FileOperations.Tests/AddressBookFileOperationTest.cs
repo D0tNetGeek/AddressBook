12162405d19815ac7013b89aa79bbf85fb9e12a6
Vinnie Malvi <vinod.malvi@yahoo.co.uk>
2019-10-28 02:42:05 +0000

    Added file operations tests

--- AddressBook.FileOperations.Tests/AddressBookFileOperationTest.cs
+++ AddressBook.FileOperations.Tests/AddressBookFileOperationTest.cs
@@ -1,14 +1,54 @@
-using System;
+using Moq;
 using Xunit;
 
 namespace AddressBook.FileOperations.Tests
 {
-    public class UnitTest1
+    public class AddressBookFileOperationTest
     {
-        [Fact]
-        public void Test1()
-        {
+        private IAddressBookFileOperations _addressBookFileOperations;
 
+        public AddressBookFileOperationTest()
+        {
+            var addressBookLines = new string[]
+            {
+                "John Snow,Male,16/03/77",
+                "Jimmy Neutron,Male,15/01/85",
+                "Dana Lane,Female,20/11/91",
+                "Sarah Connor,Female,20/09/80",
+                "Chuck Jackson,Male,14/08/74"
+            };
+            var fileReaderMock = new Mock<IFileReader>();
+
+            fileReaderMock.Setup(fileReader => fileReader.GetAllLines(It.IsAny<string>())).Returns(addressBookLines);
+
+            _addressBookFileOperations = new AddressBookFileOperation(fileReaderMock.Object);
+        }
+
+        [Fact]
+        public void GetAllContacts_InputIsFilePath_ReturnContacts()
+        {
+            var contacts = _addressBookFileOperations.GetAllContactLines();
+
+            Assert.IsType<ContactDto>(contacts.First());
+        }
+
+        [Fact]
+        public void GetAllContacts_InputIsFilePath_ReturnFourContacts()
+        {
+            var contacts = _addressBookFileOperations.GetAll();
+
+            Assert.Equal(5, contacts.Count);
+        }
+
+        [Fact]
+        public void GetAllContacts_InputIsFilePath_ShouldMapPropertiesProperly()
+        {
+            var contacts = _addressBookFileOperations.GetAll();
+            var firstContact = contacts.First();
+
+            Assert.Equal("John Snow", firstContact.Name);
+            Assert.Equal(Gender.Male, firstContact.Gender);
+            Assert.Equal("16/03/77", firstContact.BirthDate.ToString("dd/MM/yy"));
         }
     }
 }
