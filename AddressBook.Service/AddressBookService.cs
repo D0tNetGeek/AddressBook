e5b05479abfd8d7289797d6b5f068206eeab1e7c
Vinnie Malvi <vinod.malvi@yahoo.co.uk>
2019-10-28 03:07:05 +0000

    Added Service interdace

--- AddressBook.Service/AddressBookService.cs
+++ AddressBook.Service/AddressBookService.cs
@@ -2,7 +2,7 @@
 
 namespace AddressBook.Service
 {
-    public class Class1
+    public class AddressBookService
     {
     }
 }
---
+++ AddressBook.Service/IAddressBookService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using AddressBook.Service.Models;
+
+namespace AddressBook.Service
+{
+    public interface IAddressBookService
+    {
+        int CountPersonsBasedOnGender(Gender gender, IList<ContactDto> contacts);
+        ContactDto GetOldestPerson(IList<ContactDto> contacts);
+        int GetDaysBetweenTwoPersons(ContactDto contact1, ContactDto contact2);
+    }
+}
