6e75560a6a008674420ff92e00dbcd325d9d4f72
Vinnie Malvi <vinod.malvi@yahoo.co.uk>
2019-10-28 03:02:57 +0000

    Added contact dto

---
+++ AddressBook.Service/Models/ContactDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AddressBook.Service.Models
+{
+    public class ContactDto : IComparable<ContactDto>
+    {
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public int CompareTo(ContactDto contact)
+        {
+            if (this.BirthDate > contact.BirthDate) return -1;
+            if (this.BirthDate < contact.BirthDate) return 0;
+            return 1;
+        }
+    }
+}
